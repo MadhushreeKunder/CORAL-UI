1763f9586b08e90717c1e572e340115114150aba
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-12 02:40:45 +0530

    responsive hamburger click open close

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ function showToast() {
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
   }
+
+  function classToggle() {
+    const navs = document.querySelectorAll('.side-navigation')
+    
+    navs.forEach(nav => nav.classList.toggle('side-navToggleShow'));
+  }
+  
+  document.querySelector('.hamburger-menu--toggle')
+    .addEventListener('click', classToggle);
